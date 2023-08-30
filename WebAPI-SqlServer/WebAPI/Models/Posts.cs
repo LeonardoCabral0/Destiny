0b3bcc6bf307934aa98ecdd61a1684059e04d4d5
LeonardoCabral0 <leonardo.cabral5@etec.sp.gov.br>
2023-08-30 11:21:35 -0300

    Ajustando o limite da descrição

--- WebAPI-SqlServer/WebAPI/Models/Posts.cs
+++ WebAPI-SqlServer/WebAPI/Models/Posts.cs
@@ -3,11 +3,12 @@
     public class Post
     {
         public int postId { get; set; }
-        public string? Nome { get; set; }
-        public string? UF { get; set; }
-        public string? Cidade { get; set; }
+        public string Nome { get; set; }
+        public string UF { get; set; }
+        public string Cidade { get; set; }
         public string? Referencia { get; set; }
-        public string? Descricao { get; set; }
+        [StringLength(100)]
+        public string Descricao { get; set; }
         public DateTime Data { get; set; }
     }
 }
